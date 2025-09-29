47194788d44e371c2bd07e0c19af8437e4eda79d
Tib <NotAn@ema.il>
2025-09-29 08:09:17 +0200

    save prediction benchmark

--- 2_benchmark/benchmark.py
+++ 2_benchmark/benchmark.py
@@ -10,9 +10,11 @@ print("cuda" if torch.cuda.is_available() else "cpu")
 
 models = [lambda f: qa_squad(f, "what is the full name of the proposed machine learning model ? It is not in the references")]
 
+df = pd.read_csv("data/files/train.csv")
 
 data = {
-    "Model": [],
+    "Model_predicted": [],
+    "Model_true": []
 }
 
 
@@ -28,7 +30,13 @@ for i in range(1000):
     if os.path.exists(f"data/files/train/{i}.txt"):
         print(f"File {i}.txt exists")
         with open(f"data/files/train/{i}.txt", "r") as f:
-            data["Model"].append(qa_squad(f.read(), "what is the full name of the proposed model ? It's not in the references"))
-        print(pd.DataFrame(data).to_string(max_rows=None, max_cols=None))
+            data["Model_predicted"].append(qa_squad(f.read(), "what is the full name of the proposed model ? It's not in the references"))
+            data["Model_true"].append(df.iloc[i]["Model"])
+        #print(pd.DataFrame(data).to_string(max_rows=None, max_cols=None))
     else:
         print(f"File {i}.txt does not exist")
+
+    print(pd.DataFrame(data).to_string(max_rows=None, max_cols=None))
+
+    df_data = pd.DataFrame(data)
+    df_data.to_csv("2_banchmark/prediction.csv", index=False)
