70f7299306257fec5dd0faa2b5451b8fc97ff1f7
Tib <NotAn@ema.il>
2025-09-30 09:02:36 +0200

    try new benchmark

--- 2_benchmark/benchmark.py
+++ 2_benchmark/benchmark.py
@@ -11,7 +11,7 @@ print("cuda" if torch.cuda.is_available() else "cpu")
 #models = [lambda f: qa_squad(f, "what is the name of the proposed model ?")]
 
 
-algoriths = {
+algorithms = {
     "big_bird": {
         "Model": lambda f: qa_squad(f, "what is the name of the proposed model ?", "FredNajjar/bigbird-QA-squad_v2.3"),
         "Parameters": lambda f: qa_squad(f, "what is the number of parameters of the proposed model ?", "FredNajjar/bigbird-QA-squad_v2.3"),
@@ -29,7 +29,7 @@ df = pd.read_csv("data/tables/train.csv")
 #}
 
 
-keys = ["Model", "Parameters", "Training hardware"]
+#keys = ["Model", "Parameters", "Training hardware"]
 
 #data = {
 #    "Model" : {
@@ -51,17 +51,48 @@ keys = ["Model", "Parameters", "Training hardware"]
 #    }
 #}
 
-data = {key: {
+#data = {key: {
+#    "true": [],
+#    "predicted": [],
+#    "other": []
+#} for key in keys}
+
+
+results = {algorithm: {key: {
     "true": [],
     "predicted": [],
     "other": []
-} for key in keys}
+} for key in algorithms[algorithm].keys()} for algorithm in algorithms}
+
+for i in range(len(df)):
+    for algorithm in algorithms:
+        for key in algorithms[algorithm].keys():
+            results[algorithm][key]["true"].append(df.iloc[i][key])
+            if os.path.exists(f"data/files/train/{i}.txt"):
+                with open(f"data/files/train/{i}.txt", "r") as f:
+                    output = algorithms[algorithm][key](f.read())
+                    results[algorithm][key]["predicted"].append(output[0])
+                    results[algorithm][key]["other"].append(output[1])
+            else:
+                results[algorithm][key]["predicted"].append("")
+                results[algorithm][key]["other"].append("")
+            
+            res_per_algo_key = pd.DataFrame(results[algorithm][key])
+            res_per_algo_key.to_csv(f"2_benchmark/results/{algorithm}_{key}.csv", index=False)
+
+            #keys = [f"{key}_{suffix}" for key in keys for suffix in ("true", "predicted")]
 
 
+            res_per_algo = {}
+            for key in results[algorithm].keys():
+                res_per_algo[key+"_true"] = results[algorithm][key]["true"]
+                res_per_algo[key+"_predicted"] = results[algorithm][key]["predicted"]
+            
+            res_per_algo = pd.DataFrame(res_per_algo)
+            res_per_algo.to_csv(f"2_benchmark/results/{algorithm}.csv", index=False)
 
-df_data = pd.
 
-
+            
 
 
 
@@ -70,27 +101,27 @@ df_data = pd.
 ###################################
 
 
-for i in range(1000):
-    if os.path.exists(f"data/files/train/{i}.txt"):
-        print(f"File {i}.txt exists")
-        with open(f"data/files/train/{i}.txt", "r") as f:
-            try: 
-                (best, other) = qa_squad(f.read(), "What is the name of the proposed model ?")
-            except Exception as e:
-                (best, other) = ("", "")
-                print(f"Error: {e}")
-                continue
-
-            
-            data["Model_predicted"].append(best)
-            data["other_predicted"].append(other)
-            data["Model_true"].append(df.iloc[i]["Model"])
-        #print(pd.DataFrame(data).to_string(max_rows=None, max_cols=None))
-    else:
-        print(f"File {i}.txt does not exist")
-
-    print(pd.DataFrame(data).to_string(max_rows=None, max_cols=None))
-
-    df_data = pd.DataFrame(data)
-
-    df_data.to_csv("2_benchmark/prediction.csv", index=False)
+#for i in range(1000):
+#    if os.path.exists(f"data/files/train/{i}.txt"):
+#        print(f"File {i}.txt exists")
+#        with open(f"data/files/train/{i}.txt", "r") as f:
+#            try: 
+#                (best, other) = qa_squad(f.read(), "What is the name of the proposed model ?")
+#            except Exception as e:
+#                (best, other) = ("", "")
+#                print(f"Error: {e}")
+#                continue
+#
+#            
+#            data["Model_predicted"].append(best)
+#            data["other_predicted"].append(other)
+#            data["Model_true"].append(df.iloc[i]["Model"])
+#        #print(pd.DataFrame(data).to_string(max_rows=None, max_cols=None))
+#    else:
+#        print(f"File {i}.txt does not exist")
+#
+#    print(pd.DataFrame(data).to_string(max_rows=None, max_cols=None))
+#
+#    df_data = pd.DataFrame(data)
+#
+#    df_data.to_csv("2_benchmark/prediction.csv", index=False)
